b256bdfe3343af87489e38633136ac1274eeeecd
Waqar Khan <kwaqar816@gmail.com>
2024-03-30 00:26:24 +0530

    commiting from master to remote dev branch

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ def make_double(input_value=None):
 output = make_double(input_value="Waqar")
 print(output)
 print(type(output))
+
+
+def make_tripple(input_value=None):
+    output = input_value * 3
+    return output
+output = make_tripple(input_value="Waqar")
+print(output)
+print(type(output))
