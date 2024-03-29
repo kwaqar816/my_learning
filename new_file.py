e98385c79494e89f1211be55f507981f7b5ac5e7
Waqar Khan <kwaqar816@gmail.com>
2024-03-30 01:11:25 +0530

    pushing from first-branch

---
+++ new_file.py
@@ -0,0 +1,7 @@
+def function_to_convert_string(input_value=None):
+    output = str(input_value)
+    return output
+
+output = function_to_convert_string(input_value=23)
+print(output)
+print(type(output))
